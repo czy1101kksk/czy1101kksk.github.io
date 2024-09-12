f3e8e29cbe142130c65244166ed16181a3873780
BRIGHT_CZY <532651226@qq.com>
2024-09-12 08:40:11 +0800

    2024/9/12

--- docs/cpp/test1.cpp
+++ docs/cpp/test1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <climits>
+#include <cmath>
+#include <cstdarg>
 
 
 int main()
@@ -32,3 +34,46 @@ int main()
 
     return 0;
 }
+
+int * find_int(int array[], int key, int array_len){
+    for(int i=0 ; i < array_len ; i++){
+        if(array[i] == key){ 
+            return &array[i];
+        }
+    }
+    return NULL;
+}
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void zero_array(int array[], int array_len){
+    int i=array_len;
+    while(i > 0){
+        array[i--] = 0;
+    }
+}
+
+long factorial(int n){
+    return n <= 1 ? 1 : n * factorial(n-1);  // 分段使用 ?:语法
+}
+
+int f(int n){
+    return n <= 2 ? 1 : f(n-1) + f(n-2);
+}
+
+// #include <cstdarg>
+float sum(int n_value, ...){
+    va_list var_arg;
+    float sum{0};
+    va_start(var_arg, n_value);
+    for(int i=0; i < n_value; i++){
+        sum += va_arg(var_arg, int);
+    }
+    va_end(var_arg);
+    return sum;
+}
+
